a780106a0e3d8e2617fe23db5d0accb593be730d
mkreive <mkreive@gmail.com>
2023-09-04 11:23:21 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        printBonusDatesBetween(2010, 2015);
+
+    }
+
+    public static void printBonusDatesBetween(int fromYear, int toYear) {
+        System.out.println(fromYear);
+        System.out.println(toYear);
+    }
+}
